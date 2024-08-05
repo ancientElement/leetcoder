7fb6a7f3e85e66b20bd51617fe1ccb1ffdb4b075
来犀 <chenbingyou.cby@alibaba-inc.com>
2024-08-05 19:31:41 +0800

    ...

--- 343.整数拆分.py
+++ 343.整数拆分.py
@@ -7,10 +7,12 @@
 # @lc code=start
 class Solution(object):
     def integerBreak(self, n):
-        dp = [0 for i in range(n+1)]
-        dp[0],dp[1] = 1,1
+        dp = [ 0 for _ in range(n+1) ]
+        dp[0],dp[1] = 0,0
         for i in range(2,n+1):
             for j in range(1,i):
-        
+                dp[i] = max(dp[i],j*(i-j),j*dp[i-j])
+        return dp[n]
+
 # @lc code=end
 
