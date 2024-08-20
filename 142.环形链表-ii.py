e725a8cfd022760d0631625e79158649d237de42
来犀 <chenbingyou.cby@alibaba-inc.com>
2024-08-20 21:11:01 +0800

    。。。

--- 142.环形链表-ii.py
+++ 142.环形链表-ii.py
@@ -6,17 +6,41 @@
 
 # @lc code=start
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution(object):
     def detectCycle(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
+        p1,p2 = head,head
+        while p1 != p2:
+            p1 = p1.next
+            if not p1: return None
+            if not p2.next: return None
+            p2 = p2.next.next
+            if not p2: return None
+        p1 = head
+        while p1 != p2:
+            p1 = p1.next
+            p2 = p2.next
+        return p1
         
 # @lc code=end
-
+# @km code=start
+while(True):
+    try:
+        arr = [3,2,0,-4]
+        tail_to = 1
+        tail_to_node = None
+        head = None
+        cur = head
+        for i in range(len(arr)):
+            cur = ListNode(arr[i])
+            if i == 1: head = cur
+            if tail_to == i: tail_to_node = cur
+            cur = cur.next
+            
+        
+    except:
+# @km code=end
